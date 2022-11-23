da1b878f0dc70f753a4c62a081950a3417f7546e
Jaskarn <jaskarn15@gmail.com>
2022-11-23 00:19:02 +0000

    add code to limit SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,17 +9,49 @@ const pool = new Pool({
 
 let input = process.argv;
 
-
-pool.query(`
+const queryString = `
 SELECT students.id AS id, students.name AS name, cohorts.name AS cohort_name
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${input[2]}%'
-LIMIT ${input[3]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = input[2];
+const limit = input[3];
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`)
   })
 })
 .catch(err => console.error('query error', err.stack));
+
+
+// pool.query(`
+// SELECT students.id AS id, students.name AS name, cohorts.name AS cohort_name
+// FROM students
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${input[2]}%'
+// LIMIT ${input[3]};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`)
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT students.id AS id, students.name AS name, cohorts.name AS cohort_name
+// FROM students
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${input[2]}%'
+// LIMIT ${input[3]};
+// `)
+// .then(res => {
+//   console.log(res.rows);
+//   })
+// .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,18 +9,53 @@ const pool = new Pool({
  
 let input = process.argv;
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM cohorts
 JOIN students ON cohorts.id = students.cohort_id
 JOIN assistance_requests ON students.id = assistance_requests.student_id
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
-WHERE cohorts.name LIKE '%${input[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`;
+
+const cohortName = input[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values) 
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
+
+
+// pool.query(`
+// SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+// FROM cohorts
+// JOIN students ON cohorts.id = students.cohort_id
+// JOIN assistance_requests ON students.id = assistance_requests.student_id
+// JOIN teachers ON assistance_requests.teacher_id = teachers.id
+// WHERE cohorts.name LIKE '%${input[2]}%'
+// ORDER BY teachers.name;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   console.log(res.rows);
+// });
